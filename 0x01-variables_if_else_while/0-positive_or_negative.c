20ef9028aaa8d83a5e8b90d0ac57eb65c6c75706
“Veronica <thisisveronicawambui@gmail.com>
2022-10-17 13:29:52 -0700

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,9 @@
 
 /**
  * main - assign a random number to the variable n
- * if the number is zero, positive, negative
+ * if the number is greater than 0: is positive
+ * if the number is 0: is zero
+ * if the number is less than 0: is negative
  * Return: 0
  * /
 
