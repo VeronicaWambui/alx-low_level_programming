0371851fce37807386245ef6c7cc1b807430da4e
“Veronica <thisisveronicawambui@gmail.com>
2022-10-21 19:59:05 -0700

    9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,11 +17,11 @@ int main(void)
 		else if (num % 3 == 0 && num % 5 != 0)
 			printf("Fizz ");
 		else if (num == 100)
-			printf("Buzz")
+			printf("Buzz");
 		else if (num % 5 == 0 && num % 3 != 0)
 			printf("Buzz ");
 		else
-			printf("%d ", num)
+			printf("%d ", num);
 	}
 	printf("\n");
 	return (0);
