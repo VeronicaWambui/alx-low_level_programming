641b370b422c0b4fd18297f88a286920c1598f3b
“Veronica <thisisveronicawambui@gmail.com>
2022-10-28 08:59:09 -0700

    2. strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *  _strncpy -  a function that copies a string
+ *  @dest: pointer to destination char
+ *  @src: pointer to source char
+ *  @n: number of bytes
+ *  Return: char
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int v;
+
+	v = 0;
+	while (v < n && src[j] != '\0')
+	{
+		dest[v] = src [v];
+		v++;
+	}
+	while (v < n)
+	{
+		dest[v] = '\0';
+		v++;
+	}
+	return (dest);
+}
