859e12476ebe965c2f79d052196c369155f96113
VeronicaWambui <106995110+VeronicaWambui@users.noreply.github.com>
2023-01-16 13:04:24 +0300

    0x18-dynamic_libraries

---
+++ 0x18-dynamic_libraries/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "main.h"
+int main(void)
+{
+	//_puts("\"At the end of the day\"\n\t - David Efod");
+	int name;
+	name = mul(2, 3);
+	printf("%d\n", name);
+	return (0);
+}
